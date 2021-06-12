1e7193cf951cc145ab067c9b1f51494ae98bf48d
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-06-12 21:44:02 +0900

    3箇所のエラーを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def index
-    @item = Item.order(created_at: :desc)
+    # @item = Item.order(created_at: :desc)
+    @items = Item.order(created_at: :desc)
   end
 
   def new
@@ -8,6 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    # binding.pry
     Item.create!(item_params)
     redirect_to items_path
   end
@@ -15,6 +17,22 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:items).permit(:name, :price)
+    # params.require(:items).permit(:name, :price)
+    params.require(:item).permit(:name, :price)
   end
 end
+
+# エラー1
+# @items => nil
+# 場所：items_controller.rb
+# indexアクションを @items = Item.order(created_at: :desc)に修正
+
+# エラー2
+# ActionController::ParameterMissing: param is missing or the value is empty: items
+# 場所：items_controller.rb ストロングパラメータ修正
+# .require(:item)に変更
+
+# エラー3
+# Unpermitted parameter: :nickname
+# 場所：_form.html.erb
+# :nicknameを:nameに修正
